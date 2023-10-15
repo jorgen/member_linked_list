f95585ae9ccc58f5f837ee05d3220263359dbbe1
Jørgen Lind <jorgen.lind@gmail.com>
2023-10-16 00:42:19 +0200

    Reindent, and change insert slightly

--- include/memberlinkedlist.h
+++ include/memberlinkedlist.h
@@ -32,252 +32,264 @@ template <typename T>
 class MemberListNode
 {
 public:
-    MemberListNode()
-        : _next()
-        , _prev()
-    {
-    }
+  MemberListNode()
+    : _next()
+    , _prev()
+  {
+  }
 
-    MemberListNode(MemberListNode const&) = delete;
-    MemberListNode& operator=(MemberListNode const&) = delete;
+  MemberListNode(MemberListNode const&) = delete;
+  MemberListNode& operator=(MemberListNode const&) = delete;
 
-    T *next() const { return _next; }
-    T *previous() const { return _prev; }
+  T* next() const { return _next; }
+  T* previous() const { return _prev; }
 private:
-    T *_next;
-    T *_prev;
-    template<typename U, MemberListNode<U> U::*>
-    friend class MemberList;
+  T* _next;
+  T* _prev;
+  template<typename U, MemberListNode<U> U::*>
+  friend class MemberList;
 };
 
 template <typename T, MemberListNode<T> T::* Member>
 class MemberList
 {
 public:
-    class iterator
+  class iterator
+  {
+    typedef std::bidirectional_iterator_tag iterator_category;
+  public:
+    iterator()
+      : current()
+      , list()
+    {}
+
+    iterator(T* data, const MemberList* list)
+      : current(data)
+      , list(list)
+    {}
+
+    iterator& operator++()
     {
-        typedef std::bidirectional_iterator_tag iterator_category;
-    public:
-        iterator()
-            : current()
-            , list()
-        {}
+      assert(current);
+      current = (current->*Member).next();
+      return *this;
+    }
 
-        iterator(T *data, const MemberList *list)
-            : current(data)
-            , list(list)
-        {}
-
-        iterator& operator++()
-        {
-            assert(current);
-            current = (current->*Member).next();
-            return *this;
-        }
-
-        iterator operator++(int)
-        {
-            assert(current);
-            MemberList::iterator self = *this;
-            current = (current->*Member).next();
-            return self;
-        }
-
-        iterator& operator--()
-        {
-            if (current)
-                current = (current->*Member).previous();
-            else if (list)
-                current = list->_last;
-            return *this;
-        }
-
-        iterator operator--(int)
-        {
-            MemberList::iterator self = *this;
-            if (current)
-                current = (current->*Member).previous();
-            else if (list)
-                current = list->_last;
-            return *this;
-        }
-
-        bool operator==(const iterator &other) const
-        {
-            return current == other.current;
-        }
-
-        bool operator!=(const iterator &other) const
-        {
-            return current != other.current;
-        }
-
-        T *operator->() const
-        {
-            return current;
-        }
-
-        MemberListNode<T> operator*() const
-        {
-            return current->*Member;
-        }
-    private:
-        T *current;
-        const MemberList *list;
-        friend class MemberList;
-    };
-
-    MemberList()
-        : _first()
-        , _last()
-    { }
-
-    void push_front(T *data)
+    iterator operator++(int)
     {
-        assert(data);
-        if (_first)
-          (_first->*Member)._prev = data;
-        (data->*Member)._next = _first;
+      assert(current);
+      MemberList::iterator self = *this;
+      current = (current->*Member).next();
+      return self;
+    }
+
+    iterator& operator--()
+    {
+      if (current)
+        current = (current->*Member).previous();
+      else if (list)
+        current = list->_last;
+      return *this;
+    }
+
+    iterator operator--(int)
+    {
+      MemberList::iterator self = *this;
+      if (current)
+        current = (current->*Member).previous();
+      else if (list)
+        current = list->_last;
+      return *this;
+    }
+
+    bool operator==(const iterator& other) const
+    {
+      return current == other.current;
+    }
+
+    bool operator!=(const iterator& other) const
+    {
+      return current != other.current;
+    }
+
+    T* operator->() const
+    {
+      return current;
+    }
+
+    MemberListNode<T> operator*() const
+    {
+      return current->*Member;
+    }
+  private:
+    T* current;
+    const MemberList* list;
+    friend class MemberList;
+  };
+
+  MemberList()
+    : _first()
+    , _last()
+  { }
+
+  void push_front(T* data)
+  {
+    assert(data);
+    if (_first)
+      (_first->*Member)._prev = data;
+    (data->*Member)._next = _first;
+    _first = data;
+    (data->*Member)._prev = nullptr;
+    if (!_last)
+      _last = data;
+  }
+
+  void prepend(T* data) { push_front(data); }
+
+  void pop_front()
+  {
+    assert(_first);
+    T* next = (_first->*Member)._next;
+    (_first->*Member)._next = nullptr;
+    (_first->*Member)._prev = nullptr;
+    _first = next;
+    if (_first)
+      (_first->*Member)._prev = nullptr;
+    else
+      _last = nullptr;
+  }
+
+  void push_back(T* data)
+  {
+    (data->*Member)._prev = _last;
+    if (_last)
+      (_last->*Member)._next = data;
+    (data->*Member)._next = nullptr;
+    _last = data;
+    if (!_first)
+      _first = data;
+  }
+
+  void append(T* data) { push_back(data); }
+
+  void pop_back()
+  {
+    assert(_last);
+    T* newLast = (_last->*Member)._prev;
+    (_last->*Member)._prev = nullptr;
+    if (newLast)
+    {
+      _last = newLast;
+      (newLast->*Member)._next = nullptr;
+    }
+    else
+    {
+      _last = nullptr;
+      _first = nullptr;
+    }
+
+  }
+
+  static const MemberListNode<T>& getListNode(const T* data)
+  {
+    return data->*Member;
+  }
+
+  static MemberListNode<T>& getListNode(T* data)
+  {
+    return data->*Member;
+  }
+
+  T* first() const { return _first; }
+  T* last() const { return _last; }
+
+  iterator insert(const iterator& it, T* data)
+  {
+    assert(it.list == this);
+
+    if (it.current)
+    {
+      MemberListNode<T>& dataNode = data->*Member;
+      MemberListNode<T>& currentNode = it.current->*Member;
+      T* prevNode = currentNode._prev;
+
+      if (prevNode) {
+        (prevNode->*Member)._next = data;
+      }
+      else {
         _first = data;
-        (data->*Member)._prev = nullptr;
-        if (!_last)
-            _last = data;
+      }
+
+      dataNode._prev = prevNode;
+      dataNode._next = it.current;
+      currentNode._prev = data;
+    }
+    else {
+      push_back(data);
     }
 
-    void prepend(T *data) { push_front(data); }
+    return iterator(data, this);
+  }
 
-    void pop_front()
-    {
-        assert(_first);
-        T *next = (_first->*Member)._next;
-        (_first->*Member)._next = nullptr;
-        (_first->*Member)._prev = nullptr;
-        _first = next;
-        if (_first)
-            (_first->*Member)._prev = nullptr;
-        else
-            _last = nullptr;
+  iterator moveList(const iterator& it, MemberList& list)
+  {
+    assert(it.list == this);
+    if (!list._first)
+      return it;
+    if (it.current) {
+      MemberListNode<T>& currentNode = it.current->*Member;
+      if (currentNode._prev)
+        (currentNode._prev->*Member)._next = list._first;
+      else
+        _first = list._first;
+      (list._first->*Member)._prev = currentNode._prev;
+      (list._last->*Member)._next = it.current;
+      currentNode._prev = list._last;
     }
-
-    void push_back(T *data)
-    {
-        (data->*Member)._prev = _last;
-        if (_last)
-            (_last->*Member)._next = data;
-       (data->*Member)._next = nullptr;
-        _last = data;
-        if (!_first)
-            _first = data;
+    else {
+      if (!_first) {
+        _first = list._first;
+        _last = list._last;
+      }
+      else {
+        (_last->*Member)._next = list._first;
+        (list._first->*Member)._prev = _last;
+        _last = list._last;
+      }
     }
+    iterator retIt(list._first, this);
+    list._first = nullptr;
+    list._last = nullptr;
+    return retIt;
+  }
 
-    void append(T *data) { push_back(data); }
+  iterator erase(const iterator& it)
+  {
+    assert(it.list == this);
+    assert(it.current);
+    MemberListNode<T>& currentNode = it.current->*Member;
+    if (currentNode._prev)
+      (currentNode._prev->*Member)._next = currentNode._next;
+    else
+      _first = currentNode._next;
+    if (currentNode._next)
+      (currentNode._next->*Member)._prev = currentNode._prev;
+    else
+      _last = currentNode._prev;
 
-    void pop_back()
-    {
-        assert(_last);
-        T *newLast = (_last->*Member)._prev;
-        (_last->*Member)._prev = nullptr;
-        if (newLast)
-        {
-            _last = newLast;
-            (newLast->*Member)._next = nullptr;
-        }
-        else
-        {   _last = nullptr;
-            _first = nullptr;
-        }
+    iterator retIt(currentNode._next, this);
+    currentNode._next = nullptr;
+    currentNode._prev = nullptr;
+    return retIt;
+  }
 
-    }
+  typedef std::reverse_iterator<iterator> reverse_iterator;
 
-    static const MemberListNode<T> &getListNode(const T *data)
-    {
-        return data->*Member;
-    }
-
-    static MemberListNode<T> &getListNode(T *data)
-    {
-        return data->*Member;
-    }
-
-    T *first() const { return _first; }
-    T *last() const { return _last; }
-
-    iterator insert(const iterator &it, T *data)
-    {
-        assert(it.list == this);
-        if (it.current) {
-            MemberListNode<T> &dataNode = data->*Member;
-            MemberListNode<T> &currentNode = it.current->*Member;
-            if (currentNode._prev)
-                (currentNode._prev->*Member)._next = data;
-            else
-                _first = data;
-            dataNode._prev = currentNode._prev;
-            dataNode._next = it.current;
-            currentNode._prev = data;
-        } else {
-            push_back(data);
-        }
-        return iterator(data, this);
-    }
-
-    iterator moveList(const iterator &it, MemberList &list)
-    {
-        assert(it.list == this);
-        if (!list._first)
-            return it;
-        if (it.current) {
-            MemberListNode<T> &currentNode = it.current->*Member;
-            if (currentNode._prev)
-                (currentNode._prev->*Member)._next = list._first;
-            else
-                _first = list._first;
-            (list._first->*Member)._prev = currentNode._prev;
-            (list._last->*Member)._next = it.current;
-            currentNode._prev = list._last;
-        } else {
-            if (!_first) {
-                _first = list._first;
-                _last = list._last;
-            } else {
-                (_last->*Member)._next = list._first;
-                (list._first->*Member)._prev = _last;
-                _last = list._last;
-            }
-        }
-        iterator retIt(list._first, this);
-        list._first = nullptr;
-        list._last = nullptr;
-        return retIt;
-    }
-
-    iterator erase(const iterator &it)
-    {
-        assert(it.list == this);
-        assert(it.current);
-        MemberListNode<T> &currentNode = it.current->*Member;
-        if (currentNode._prev)
-            (currentNode._prev->*Member)._next = currentNode._next;
-        else
-            _first = currentNode._next;
-        if (currentNode._next)
-            (currentNode._next->*Member)._prev = currentNode._prev;
-        else
-            _last = currentNode._prev;
-
-        iterator retIt(currentNode._next, this);
-        currentNode._next = nullptr;
-        currentNode._prev = nullptr;
-        return retIt;
-    }
-
-    typedef std::reverse_iterator<iterator> reverse_iterator;
-
-    iterator begin() const { return iterator(_first, this); }
-    iterator end() const { return iterator(nullptr, this); }
+  iterator begin() const { return iterator(_first, this); }
+  iterator end() const { return iterator(nullptr, this); }
 private:
-    T *_first;
-    T *_last;
+  T* _first;
+  T* _last;
 };
 
 #endif //MEMBERLINKEDLIST_H
